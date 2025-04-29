9d598d3fb7c4adabc55f2f68c14c1299d5d79f29
muniprasad <muniprasaddiddi@gmail.com>
2025-04-29 11:49:50 +0530

    files updated

--- resource.tf
+++ resource.tf
@@ -10,30 +10,30 @@ provider "azurerm" {
 #   address_prefixes     = ["10.0.2.0/24"]
 # }
 
-resource "azurerm_network_interface" "test" {
-  name                = "test-nic"
+resource "azurerm_network_interface" "demo" {
+  name                = "demo-nic"
   location            = "UK West"
   resource_group_name = "Project_RG"
 
   ip_configuration {
-    name                          = "test-nic"
+    name                          = "demo-nic"
     subnet_id                     = "/subscriptions/aa32da49-0603-4855-b55b-bfd4bcf7b16f/resourceGroups/Project_RG/providers/Microsoft.Network/virtualNetworks/Project_VN/subnets/default"
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_public_ip" "test" {
-  name                = "acceptanceTestPublicIp1"
+resource "azurerm_public_ip" "demo" {
+  name                = "demo-ip"
   resource_group_name = "Project_RG"
   location            = "UK West"
   allocation_method   = "Static"
 }
 
-resource "azurerm_virtual_machine" "test" {
-  name                  = "test-VM"
+resource "azurerm_virtual_machine" "demo" {
+  name                  = "demo-VM"
   location              = "UK West"
   resource_group_name   = "Project_RG"
-  network_interface_ids = [azurerm_network_interface.test.id]
+  network_interface_ids = [azurerm_network_interface.demo.id]
   vm_size               = "Standard_B1ms"
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
@@ -43,16 +43,16 @@ resource "azurerm_virtual_machine" "test" {
   delete_data_disks_on_termination = true
 
 storage_image_reference {
-    id = "/subscriptions/aa32da49-0603-4855-b55b-bfd4bcf7b16f/resourceGroups/Project_RG/providers/Microsoft.Compute/images/image-devops-practice"
+    id = "/subscriptions/aa32da49-0603-4855-b55b-bfd4bcf7b16f/resourceGroups/Project_RG/providers/Microsoft.Compute/images/test-devops-practice"
   }
   storage_os_disk {
-    name              = "test-vm-disk"
+    name              = "demo-vm-disk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
   os_profile {
-    computer_name  = "test-vm"
+    computer_name  = "demo-vm"
     admin_username = "azuser"
     admin_password = "Muniprasad@123"
   }
