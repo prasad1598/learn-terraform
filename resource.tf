ffe568800a25d95a68180def4cb522b8f16fb74a
muniprasad <muniprasaddiddi@gmail.com>
2025-04-29 12:49:13 +0530

    files updated

--- resource.tf
+++ resource.tf
@@ -3,31 +3,31 @@ provider "azurerm" {
   subscription_id = "aa32da49-0603-4855-b55b-bfd4bcf7b16f"
 }
 
-resource "azurerm_public_ip" "demo" {
-  name                = "demo-ip"
+resource "azurerm_public_ip" "frontend" {
+  name                = "frontend-ip"
   resource_group_name = "Project_RG"
   location            = "UK West"
   allocation_method   = "Static"
 }
 
-resource "azurerm_network_interface" "demo" {
-  name                = "demo-nic"
+resource "azurerm_network_interface" "frontend" {
+  name                = "frontend-nic"
   location            = "UK West"
   resource_group_name = "Project_RG"
 
   ip_configuration {
-    name                          = "demo-nic"
+    name                          = "frontend-nic"
     subnet_id                     = "/subscriptions/aa32da49-0603-4855-b55b-bfd4bcf7b16f/resourceGroups/Project_RG/providers/Microsoft.Network/virtualNetworks/Project_VN/subnets/default"
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.demo.id
+    public_ip_address_id          = azurerm_public_ip.frontend.id
   }
 }
 
-resource "azurerm_virtual_machine" "demo" {
-  name                = "demo"
+resource "azurerm_virtual_machine" "frontend" {
+  name                = "frontend"
   location            = "UK West"
   resource_group_name = "Project_RG"
-  network_interface_ids = [azurerm_network_interface.demo.id]
+  network_interface_ids = [azurerm_network_interface.frontend.id]
   vm_size = "Standard_B1ms"
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
@@ -40,13 +40,13 @@ resource "azurerm_virtual_machine" "demo" {
     id = "/subscriptions/aa32da49-0603-4855-b55b-bfd4bcf7b16f/resourceGroups/Project_RG/providers/Microsoft.Compute/images/test-devops-practice"
   }
   storage_os_disk {
-    name              = "demo-vm-disk"
+    name              = "frontend-vm-disk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
   os_profile {
-    computer_name  = "demo-vm"
+    computer_name  = "frontend-vm"
     admin_username = "azuser"
     admin_password = "Muniprasad@123"
   }
