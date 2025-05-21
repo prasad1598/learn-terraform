9b4603e4d218a9f359d3ed00df50816ab36b0673
Muniprasad <muniprasaddiddi@gmail.com>
2025-05-21 22:15:21 +0530

    roboshop project update

---
+++ Vault/main.tf
@@ -0,0 +1,15 @@
+data "vault_generic_secret" "ssh" {
+  path = "roboshop-infra/ssh"
+}
+
+provider "vault" {
+  address = "http://vault-int.prasaddevops.shop:8200"
+  token   = var.token
+}
+
+resource "local_file" "foo" {
+  content  = data.vault_generic_secret.ssh.data
+  filename = "/tmp/vault"
+}
+
+variable "token" {}
