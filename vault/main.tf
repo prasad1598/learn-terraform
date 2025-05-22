d0e1fc475bdb9287c646ba0289d661d9347dd52a
Muniprasad <muniprasaddiddi@gmail.com>
2025-05-22 10:56:23 +0530

    roboshop project update

--- vault/main.tf
+++ vault/main.tf
@@ -7,7 +7,7 @@ data "vault_generic_secret" "ssh" {
   path = "roboshop-infra/ssh"
 }
 
-resource "local_file" "foo" {
+resource "local_file" "ssh" {
   content  = data.vault_generic_secret.ssh.data["password"]
   filename = "/tmp/vault"
 }
