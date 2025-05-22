447673ffde27237a6693f2305401249997a31967
Muniprasad <muniprasaddiddi@gmail.com>
2025-05-22 12:56:35 +0530

    roboshop project update

--- remote-state/tfstate.tf
+++ remote-state/tfstate.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "azurerm" {
+    use_cli              = true
     subscription_id      = "aa32da49-0603-4855-b55b-bfd4bcf7b16f"
     resource_group_name  = "project_rg"
     storage_account_name = "terratfstateform"
     container_name       = "roboshop-tfstate-files"
-    key                  = "test.terraform.tfstate"
+    key                  = "vault.terraform.tfstate"
   }
 }
